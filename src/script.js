dec8db0ac51a7bba78451f07de8de3fa9124c9b2
Alicia Buelow <alicia@buelow.com.au>
2026-01-22 14:13:24 +0800

    Removed default cities and dropdown select is functional

--- src/script.js
+++ src/script.js
@@ -1,19 +1,24 @@
-function torontoLocalTime() {
-  let torontoDateElement = document.querySelector("#toronto-date");
-  torontoDateElement.innerHTML = moment().tz("America/Toronto").format("LL");
-  let torontoTimeElement = document.querySelector("#toronto-time");
-  torontoTimeElement.innerHTML = moment()
-    .tz("America/Toronto")
-    .format("HH:mm:ss");
-}
-setInterval(torontoLocalTime, 1000);
+//select cities
 
-function perthLocalTime() {
-  let perthDateElement = document.querySelector("#perth-date");
-  perthDateElement.innerHTML = moment().tz("Australia/Perth").format("LL");
-  let perthTimeElement = document.querySelector("#perth-time");
-  perthTimeElement.innerHTML = moment()
-    .tz("Australia/Perth")
-    .format("HH:mm:ss");
+function selectCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityDate = moment().tz(cityTimeZone).format("LL");
+  let cityTime = moment().tz(cityTimeZone).format("HH:mm:ss");
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML += `<div class="city">
+          <div>
+            <div class="city-name"><h2>${cityName}</h2></div>
+            <div class="date">${cityDate}</div>
+          </div>
+          <div class="time">${cityTime}</div>
+        </div>`;
+
+  function refreshTime() {
+    let cityTime = moment().tz(cityTimeZone).format("HH:mm:ss");
+  }
+  setInterval(refreshTime, 1000);
 }
-setInterval(perthLocalTime, 1000);
+
+let citySelectElement = document.querySelector("#select-city");
+citySelectElement.addEventListener("change", selectCity);
