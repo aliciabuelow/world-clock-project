bd0884b2c216aef8885f2c22927d9c58eb23c0df
Alicia Buelow <alicia@buelow.com.au>
2026-01-22 14:47:08 +0800

    Added default cities and link back to homepage

--- src/script.js
+++ src/script.js
@@ -1,5 +1,28 @@
-//select cities
+//default cities
+function refreshTime() {
+  let vancouverDateElement = document.querySelector("#vancouver-date");
+  let vancouverTimeElement = document.querySelector("#vancouver-time");
+  let vancouverTime = moment().tz("America/Vancouver");
+  vancouverDateElement.innerHTML = vancouverTime.format("LL");
+  vancouverTimeElement.innerHTML = vancouverTime.format("HH:mm:ss");
 
+  let londonDateElement = document.querySelector("#london-date");
+  let londonTimeElement = document.querySelector("#london-time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("LL");
+  londonTimeElement.innerHTML = londonTime.format("HH:mm:ss");
+
+  let sydneyDateElement = document.querySelector("#sydney-date");
+  let sydneyTimeElement = document.querySelector("#sydney-time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("LL");
+  sydneyTimeElement.innerHTML = sydneyTime.format("HH:mm:ss");
+}
+
+refreshTime();
+setInterval(refreshTime, 1000);
+
+//select city function
 function selectCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
@@ -9,19 +32,15 @@ function selectCity(event) {
   let cityDate = moment().tz(cityTimeZone).format("LL");
   let cityTime = moment().tz(cityTimeZone).format("HH:mm:ss");
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML += `<div class="city">
+  citiesElement.innerHTML = `<div class="city">
           <div>
             <div class="city-name"><h2>${cityName}</h2></div>
             <div class="date">${cityDate}</div>
           </div>
           <div class="time">${cityTime}</div>
-        </div>`;
-
-  function refreshTime() {
-    let cityTime = moment().tz(cityTimeZone).format("HH:mm:ss");
-  }
-  setInterval(refreshTime, 1000);
+        </div>
+        <a href="/">Back to Home</a>`;
 }
-
+//change city
 let citySelectElement = document.querySelector("#select-city");
 citySelectElement.addEventListener("change", selectCity);
